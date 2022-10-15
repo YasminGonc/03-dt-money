41d6dd71e0003263168df827ab3e141769138032
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-10-14 21:08:19 -0300

    styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionContainer, TrasactionsTable } from "./styles";
 
 export function Transactions() {
-    return(
+    return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionContainer>
+                <SearchForm />
+                <TrasactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width='50%'>Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TrasactionsTable>
+
+            </TransactionContainer>
+
         </div>
     )
 }
