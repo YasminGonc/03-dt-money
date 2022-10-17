56b2035e4e84f0bea805c27dbc236ac1fc0c8038
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-10-17 09:34:31 -0300

    finishing

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,29 +1,32 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSumarry() {
   const transactions = useContextSelector(TransactionContext, (context) => {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.price
+          accumulator.total += transaction.price
+        } else {
+          accumulator.outcome += transaction.price
+          accumulator.total -= transaction.price
+        }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
